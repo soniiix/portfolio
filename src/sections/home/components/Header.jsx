ec143d36dc72feb0973eb181fe998e2a6534bcda
soniiix <quent.couzinet@gmail.com>
2025-01-31 22:34:20 +0100

    chore: separate public files from assets

--- src/sections/home/components/Header.jsx
+++ src/sections/home/components/Header.jsx
@@ -5,7 +5,7 @@ export function Header() {
     return (
         <header className="flex flex-row items-center justify-between h-25 border-b-1 border-[#4e6299] px-8 py-6 font-manrope text-white lg:px-26">
             <a href="">
-                <img className="h-10 w-10" src="/images/logo.png" />
+                <img className="h-10 w-10" src="src/assets/images/logo.png" />
             </a>
             <Navbar />
         </header>
