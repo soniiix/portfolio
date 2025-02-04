1ed6238ceac52bd867bb49821fa18f434bd7ddc3
soniiix <quent.couzinet@gmail.com>
2025-02-04 22:01:43 +0100

    docs: add jsdoc to the NavLink component

--- src/sections/home/components/NavLink.jsx
+++ src/sections/home/components/NavLink.jsx
@@ -1,6 +1,14 @@
-export function NavLink({ title, anchor = "" }) {
+/**
+ * @param {string} title The title of the link.
+ * @param {string} anchorId The id of the anchor the link refers to.
+ * @returns {React.JSX.Element}
+ */
+export function NavLink({ title, anchorId = "" }) {
     return (
-        <a href={anchor} className="rounded-[20px] px-[17px] py-[12px] font-semibold hover:bg-[#4e6299]">
+        <a
+            href={anchorId.startsWith("#") ? anchorId : `#${anchorId}`}
+            className="rounded-[20px] px-[17px] py-[12px] font-semibold hover:bg-[#4e6299]"
+        >
             {title}
         </a>
     );
