c7c76999105a360ab6a69c7d9093c65099300be8
soniiix <quent.couzinet@gmail.com>
2025-02-16 16:14:36 +0100

    fix: align buttons at the bottom of the card

--- src/sections/projects/components/ProjectCard.jsx
+++ src/sections/projects/components/ProjectCard.jsx
@@ -11,21 +11,22 @@ import { TbExternalLink } from "react-icons/tb";
  */
 export function ProjectCard({ title, description, image, technologies, github }) {
     return (
-        <div className="h-full rounded-[16px] bg-gray">
+        <div className="flex flex-col rounded-[16px] bg-gray">
             <img src={image} className="h-40 w-full rounded-t-[16px] object-cover"/>
-            <div className="flex flex-col gap-2 px-8 py-5 font-manrope">
+            <div className="flex flex-col flex-grow gap-2 px-8 py-5 font-manrope">
                 <span className="text-sm font-bold text-blue">
                     {technologies.toUpperCase()}
                 </span>
                 <h1 className="text-2xl font-bold">{title}</h1>
                 <p className="mt-2 font-semibold">{description}</p>
-                <div className="mt-6 flex flex-row items-start justify-end">
+                <div className="flex-grow"></div>
+                <div className="mt-3 flex flex-row items-center justify-end">
                     <a href={github}>
                         <IoLogoGithub size={33} color="#0788FF" />
                     </a>
                     <a
                         href="#"
-                        className="ml-3 flex flex-row items-center gap-1 rounded-[22px] border-2 px-3.5 py-1 font-semibold text-blue hover:border-blue hover:bg-blue hover:text-white"
+                        className="ml-2.5 flex flex-row items-center gap-1 rounded-[22px] border-2 px-3.5 py-1 font-semibold text-blue hover:border-blue hover:bg-blue hover:text-white"
                     >
                         <span>En savoir plus</span>
                         <TbExternalLink />
