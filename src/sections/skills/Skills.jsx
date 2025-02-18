a05dd5fe9c7a2898035cf1f5bfe0fc437202b206
soniiix <quent.couzinet@gmail.com>
2025-02-18 15:24:26 +0100

    feat: add the same bottom border as others sections

--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -2,7 +2,7 @@ import { Title } from "../home/components/Title";
 
 export function Skills() {
     return (
-        <section className="h-full w-full bg-dark-blue px-8 py-20 text-white md:px-24 lg:px-40">
+        <section className="h-full w-full bg-dark-blue px-8 py-20 text-white md:px-24 lg:px-40 border-b-1 border-[#363c44]">
             <Title size={"4xl"} title={"Mes compétences"} />
         </section>
     );
