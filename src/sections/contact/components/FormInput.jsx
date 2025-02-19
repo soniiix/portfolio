4e8a53841a8ab8458e77c0b63dadbf27472464d4
soniiix <quent.couzinet@gmail.com>
2025-02-19 14:42:26 +0100

    docs: add jsdoc for the FormInput component

--- src/sections/contact/components/FormInput.jsx
+++ src/sections/contact/components/FormInput.jsx
@@ -1,5 +1,13 @@
 import { useId } from "react";
 
+/**
+ * @param {Object} props
+ * @param {string} props.label The text that will be used for placeholder & label.
+ * @param {string} props.name The value to use for the name input attribute.
+ * @param {string} [props.autocompleteValue] The optional value to use for the autocomplete input attribute.
+ * @param {boolean} [props.textArea] Specifies whether the input will be a text area or not. False by default.
+ * @returns {JSX.Element}
+ */
 export function FormInput({ label, name, autocompleteValue = "on", textArea = false }) {
     const inputId = useId();
 
