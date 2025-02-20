957b0fd156261649fa1c9983e884135ef0a33f91
soniiix <quent.couzinet@gmail.com>
2025-02-20 15:02:02 +0100

    feat: add an error prop to the FormInput component

--- src/sections/contact/components/FormInput.jsx
+++ src/sections/contact/components/FormInput.jsx
@@ -6,12 +6,13 @@ import { useId } from "react";
  * @param {string} props.name The value to use for the name input attribute.
  * @param {string} [props.autocompleteValue] The optional value to use for the autocomplete input attribute.
  * @param {boolean} [props.textArea] Specifies whether the input will be a text area or not. False by default.
+ * @param {string} props.error A possible generated error to display.
  * @returns {JSX.Element}
  */
-export function FormInput({ label, name, autocompleteValue = "on", textArea = false }) {
+export function FormInput({ label, name, autocompleteValue = "on", textArea = false, error }) {
     const inputId = useId();
 
-    const inputClassName = "bg-gray rounded-[16px] py-3 px-6 resize-none focus:outline-2 focus:outline-blue autofill:transition-colors autofill:duration-[5000000ms]";
+    const inputClassName = "bg-gray rounded-[16px] py-3 px-6 resize-none focus:outline-2 focus:outline-blue autofill:transition-colors autofill:duration-[5000000ms] " + (error && "outline-2 outline-red-500");
 
     return (
         <div className="flex flex-col space-y-3 font-manrope font-semibold">
@@ -28,13 +29,14 @@ export function FormInput({ label, name, autocompleteValue = "on", textArea = fa
                 :
                     <input
                         id={inputId}
-                        type={label.toLowerCase() == "email" ? "email" : "text"}
+                        type="text"
                         placeholder={label}
                         name={name}
                         className={inputClassName}
                         autoComplete={autocompleteValue}
                     />
             }
+            {error && <span className="text-red-500">{error}</span>}
         </div>
     );
 }
