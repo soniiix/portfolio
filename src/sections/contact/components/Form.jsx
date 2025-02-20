370e911300773233d97ebac9174bd34e141aac03
soniiix <quent.couzinet@gmail.com>
2025-02-20 10:33:09 +0100

    feat: add a submit button to the form

--- src/sections/contact/components/Form.jsx
+++ src/sections/contact/components/Form.jsx
@@ -1,4 +1,5 @@
 import { FormInput } from "./FormInput";
+import { RiMailSendLine } from "react-icons/ri";
 
 export function Form() {
     return (
@@ -9,6 +10,11 @@ export function Form() {
             </div>
             <FormInput label="Email" name="email" autocompleteValue="email"/>
             <FormInput label="Message" name="message" textArea={true}/>
+
+            <button type="submit" className=" flex flex-row gap-2 items-center justify-center rounded-[20px] bg-blue px-[30px] py-[12px] text-center font-bold hover:cursor-pointer hover:bg-btn-hover font-manrope">
+                <span>Envoyer</span>
+                <RiMailSendLine style={{marginTop: "1px"}}/>
+            </button>
         </form>
     )
 }
