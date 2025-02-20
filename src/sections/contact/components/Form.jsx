069977cf3d4527dc34929cc02c24c1c61d931fca
soniiix <quent.couzinet@gmail.com>
2025-02-20 18:28:21 +0100

    ref: format file with Prettier

--- src/sections/contact/components/Form.jsx
+++ src/sections/contact/components/Form.jsx
@@ -6,13 +6,13 @@ export function Form() {
     const [errors, setErrors] = useState({});
     const emailRegex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
 
-    const handleInputChange = ((e) => {
+    const handleInputChange = (e) => {
         setErrors((prevErrors) => {
-            const newErrors = {...prevErrors};
+            const newErrors = { ...prevErrors };
             const inputName = e.target.name;
 
-            if (inputName === "email"){
-                if (!emailRegex.test(e.target.value)){
+            if (inputName === "email") {
+                if (!emailRegex.test(e.target.value)) {
                     newErrors.email = "Adresse email invalide.";
                     return newErrors;
                 }
@@ -20,10 +20,10 @@ export function Form() {
 
             delete newErrors[inputName];
             return newErrors;
-        })
-    })
+        });
+    };
 
-    const handleSubmit = ((e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         let newErrors = {};
         setErrors({});
@@ -33,35 +33,58 @@ export function Form() {
         let email = e.target.email.value.trim();
         let message = e.target.message.value.trim();
 
-        if (firstname.length == 0) { newErrors.firstname = "Le prénom est requis." }
-        if (lastname.length == 0) { newErrors.lastname = "Le nom est requis." }
+        if (firstname.length == 0) newErrors.firstname = "Le prénom est requis.";
+        if (lastname.length == 0) newErrors.lastname = "Le nom est requis.";
         if (email.length == 0) {
             newErrors.email = "L'email est requis.";
-        } else if (!emailRegex.test(email)){
+        } else if (!emailRegex.test(email)) {
             newErrors.email = "Adresse email invalide.";
         }
-        if (message.length == 0) { newErrors.message = "Un message est requis." }
+        if (message.length == 0) newErrors.message = "Un message est requis.";
 
-        if(Object.keys(newErrors).length > 0){
+        if(Object.keys(newErrors).length > 0) {
             setErrors(newErrors);
         }
 
         //@TODO send email
-    });
+    };
 
     return (
         <form onSubmit={handleSubmit} className="flex flex-col gap-7">
             <div className="grid grid-cols-1 gap-y-7 lg:grid-cols-2 lg:gap-x-6">
-                <FormInput label="Nom" name="lastname" autocompleteValue="family-name" error={errors.lastname} onInputChange={handleInputChange}/>
-                <FormInput label="Prénom" name="firstname" autocompleteValue="given-name" error={errors.firstname} onInputChange={handleInputChange}/>
+                <FormInput
+                    label="Nom"
+                    name="lastname"
+                    autocompleteValue="family-name"
+                    error={errors.lastname}
+                    onInputChange={handleInputChange}
+                />
+                <FormInput
+                    label="Prénom" 
+                    name="firstname"
+                    autocompleteValue="given-name" 
+                    error={errors.firstname}
+                    onInputChange={handleInputChange}
+                />
             </div>
-            <FormInput label="Email" name="email" autocompleteValue="email" error={errors.email} onInputChange={handleInputChange}/>
-            <FormInput label="Message" name="message" textArea={true} error={errors.message} onInputChange={handleInputChange}/>
-
-            <button type="submit" className=" flex flex-row gap-2 items-center justify-center rounded-[20px] bg-blue px-[30px] py-[12px] text-center font-bold hover:cursor-pointer hover:bg-btn-hover font-manrope">
+            <FormInput 
+                label="Email" 
+                name="email"
+                autocompleteValue="email"
+                error={errors.email}
+                onInputChange={handleInputChange}
+            />
+            <FormInput
+                label="Message"
+                name="message"
+                textArea={true}
+                error={errors.message}
+                onInputChange={handleInputChange}
+            />
+            <button type="submit" className="flex flex-row gap-2 items-center justify-center rounded-[20px] bg-blue px-[30px] py-[12px] text-center font-manrope font-bold hover:cursor-pointer hover:bg-btn-hover">
                 <span>Envoyer</span>
-                <RiMailSendLine style={{marginTop: "1px"}}/>
+                <RiMailSendLine style={{ marginTop: "1px" }}/>
             </button>
         </form>
-    )
+    );
 }
