f856f99671f404ce63069988c4ed8e9a1b7980de
soniiix <quent.couzinet@gmail.com>
2025-02-22 21:54:47 +0100

    ref: update navbar

--- src/sections/home/components/Navbar.jsx
+++ src/sections/home/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "./NavLink";
 export function Navbar() {
     return (
         <nav className="hidden md:flex md:flex-grid md:items-center text-base md:gap-2 lg:gap-3">
-            <NavLink title="Accueil"/>
-            <NavLink title="À propos"/>
-            <NavLink title="Réalisations"/>
-            <NavLink title="Compétences"/>
+            <NavLink title="Accueil" anchorId="home"/>
+            <NavLink title="À propos" anchorId="about"/>
+            <NavLink title="Réalisations" anchorId="projects"/>
+            <NavLink title="Expérience" anchorId="experience"/>
             <button className="rounded-[20px] bg-blue px-[30px] py-[12px] font-bold hover:cursor-pointer hover:bg-btn-hover">
                 Me contacter
             </button>
