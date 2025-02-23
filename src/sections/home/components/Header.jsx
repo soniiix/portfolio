60187ee32dd7458ed2ec5675a94bcebb9a70c6c1
soniiix <quent.couzinet@gmail.com>
2025-02-23 13:20:39 +0100

    style: reduce header height

--- src/sections/home/components/Header.jsx
+++ src/sections/home/components/Header.jsx
@@ -2,7 +2,7 @@ import { Navbar } from "./Navbar";
 
 export function Header() {
     return (
-        <header className="flex flex-row items-center justify-between h-25 border-b-1 border-[#4e6299] px-8 py-6 font-manrope text-white lg:px-26">
+        <header className="flex flex-row items-center justify-between h-22 border-b-1 border-[#4e6299] px-8 py-5 font-manrope text-white lg:px-26">
             <a href="">
                 <img className="h-10 w-10" src="src/assets/images/logo.png" />
             </a>
