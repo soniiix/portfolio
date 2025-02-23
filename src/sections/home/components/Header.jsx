5a409e42c852a89d94938168958db5af7b436656
soniiix <quent.couzinet@gmail.com>
2025-02-23 17:22:35 +0100

    fix: manage header size on large screens

--- src/sections/home/components/Header.jsx
+++ src/sections/home/components/Header.jsx
@@ -2,11 +2,13 @@ import { Navbar } from "./Navbar";
 
 export function Header() {
     return (
-        <header className="flex flex-row items-center justify-between h-22 border-b-1 border-[#4e6299] px-8 py-5 font-manrope text-white lg:px-26">
-            <a href="">
-                <img className="h-10 w-10" src="src/assets/images/logo.png" />
-            </a>
-            <Navbar />
+        <header className="flex h-22 justify-center border-b-1 border-[#4e6299]">
+            <div className="flex w-full max-w-[1920px] flex-row items-center justify-between px-8 py-5 font-manrope text-white lg:px-26">
+                <a href="">
+                    <img className="h-10 w-10" src="src/assets/images/logo.png"/>
+                </a>
+                <Navbar />
+            </div>
         </header>
     );
 }
