6288ee75bce3ad1c5af68951535ebbb10d093215
soniiix <quent.couzinet@gmail.com>
2025-02-24 22:34:28 +0100

    chore: configure default route path

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/*" element={<App />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>,
 )
