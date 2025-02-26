fd046aa88fc13641c6d699e60000e7b2223917bd
soniiix <quent.couzinet@gmail.com>
2025-02-26 12:03:37 +0100

    feat: add icon to each element and social links

--- src/sections/home/components/MobileMenu.jsx
+++ src/sections/home/components/MobileMenu.jsx
@@ -1,21 +1,32 @@
 import { useState } from "react";
+import { GrLinkedinOption } from "react-icons/gr";
+import { IoLogoGithub } from "react-icons/io";
+import { IoHome } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
+import { RiStackFill } from "react-icons/ri";
+import { MdOutlineWork } from "react-icons/md";
+import { BsWindowSidebar } from "react-icons/bs";
 
 const SECTIONS = [
     {
         title: "Accueil",
         anchorId: "#home",
+        icon: <IoHome />
     },
     {
         title: "À propos",
         anchorId: "#about",
+        icon: <FaUser/>
     },
     {
         title: "Réalisations",
         anchorId: "#projects",
+        icon: <RiStackFill style={{marginTop: "1px"}} size={18}/>
     },
     {
         title: "Expérience",
         anchorId: "#experience",
+        icon: <MdOutlineWork size={18}/>
     },
 ];
 
@@ -25,6 +36,12 @@ export function MobileMenu() {
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     }
+    
+    window.addEventListener('scroll', () => {
+        if(isOpen) {
+            toggleMenu();
+        }
+    })
 
     return (
         <>
@@ -52,27 +69,59 @@ export function MobileMenu() {
                 }`}
             >
                 <div className="flex flex-col pt-30 pb-10 h-full w-full justify-between">
-                    <div className="flex flex-col text-left">
-                        {SECTIONS.map((section) => 
+                    <ul className="font-semibold">
+                        {SECTIONS.map((section, index) => (
                             <a 
-                                onClick={toggleMenu}
+                                key={index}
                                 href={section.anchorId}
-                                className="pl-16 py-5 w-full font-semibold hover:bg-btn-hover">
-                                {section.title}
+                                onClick={toggleMenu}
+                            >
+                                <li key={index} className="flex items-start pl-10 py-5 hover:cursor-pointer hover:bg-btn-hover">
+                                    <div className="me-8 mt-1 h-3.5 w-3.5 shrink-0">
+                                        {section.icon}
+                                    </div>
+                                    {section.title}
+                                </li>  
                             </a>
-                        )}
+                            
+                        ))}
+                        <a 
+                            href="https://github.com/soniiix"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            onClick={toggleMenu}
+                        >
+                            <li className="flex items-start pl-10 py-5 hover:cursor-pointer hover:bg-btn-hover">
+                                <div className="me-8 mt-0.5 h-3.5 w-3.5 shrink-0">
+                                    <IoLogoGithub size={20}/>
+                                </div>
+                                Github
+                            </li>
+                        </a>
+                        <a 
+                            href="https://www.linkedin.com/in/quentin-couzinet/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            onClick={toggleMenu}
+                        >
+                            <li className="flex items-start pl-10.5 py-5 hover:cursor-pointer hover:bg-btn-hover">
+                                <div className="me-8 mt-0.5 h-3.5 w-3.5 shrink-0">
+                                    <GrLinkedinOption size={19}/>
+                                </div>
+                                LinkedIn
+                            </li>
+                        </a>
+                    </ul>
+
+                    <div className="flex flex-col items-center justify-center gap-6">
+                        <a 
+                            onClick={toggleMenu}
+                            href="#contact"
+                            className="rounded-[20px] bg-white hover:bg-[#e6e6e6] text-blue px-[30px] py-[12px] font-bold hover:cursor-pointer">
+                            Me contacter
+                        </a>
                     </div>
-                    <div className="flex items-center justify-center">
-                    <a 
-                        onClick={toggleMenu}
-                        href="#contact"
-                        className="rounded-[20px] bg-white text-blue px-[30px] py-[12px] font-bold hover:cursor-pointer">
-                        Me contacter
-                    </a>
-                    </div>
-                    
                 </div>
-                
             </div>
         </>
     );
